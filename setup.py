ade2a5bb1a7c6b49aa0062af4100c7a1f5dcf8fd
Kshitij (Kris) Gambhir <kshitijgambhir@dtu-fjlv762.win.dtu.dk>
2024-04-30 09:07:56 +0200

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+# Read requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='odyssey', 
     version='0.1', 
     packages = find_packages(where="src"),
-    package_dir={"": "src"}
+    package_dir={"": "src"},
+    install_requires=requirements
     )
