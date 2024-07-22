79f29cbdf875c994f1759fc76a69eef034a84e3d
pwvbutler <21472918-pwvbutler@users.noreply.gitlab.com>
2024-07-22 12:07:57 +0200

    optional logging to file

--- src/odyssey/mission/base_mission.py
+++ src/odyssey/mission/base_mission.py
@@ -11,6 +11,10 @@ import pandas as pd
 import os
 import datetime
 
+import logging
+
+LOG = logging.getLogger(__name__)
+
 class Mission(ABC):
     """
     The Mission class is the central component of the Odyssey library, representing the optimization problem at hand.
@@ -32,7 +36,8 @@ class Mission(ABC):
                  name: str,
                  funcs: list, 
                  maneuvers: list, 
-                 envelope: Union[list, np.ndarray, torch.Tensor]
+                 envelope: Union[list, np.ndarray, torch.Tensor],
+                 log_data: bool = False,
         ):  
 
         """
@@ -57,26 +62,28 @@ class Mission(ABC):
 
         self.param_dims = self.envelope.shape[0]
         self.output_dims = len(self.maneuvers)
+        self.log_data = log_data
         
         # Setup Logfile
-        self.name = name
+        if self.log_data:
+            self.name = name
 
-        log_dir = "missionlogs"
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
+            log_dir = "missionlogs"
+            if not os.path.exists(log_dir):
+                os.makedirs(log_dir)
 
-        current_time = datetime.datetime.now().strftime("%d%m%y_%H%M%S")
-        self.logfile = os.path.join(log_dir, f"{self.name}-{current_time}.csv")
+            current_time = datetime.datetime.now().strftime("%d%m%y_%H%M%S")
+            self.logfile = os.path.join(log_dir, f"{self.name}-{current_time}.csv")
 
-        # Setup Logfile Columns
-        param_list = [f"param_{i}" for i in range(1, self.param_dims + 1)]
-        objective_list = [f"objective_{i}" for i in range(1, self.output_dims + 1)]
+            # Setup Logfile Columns
+            param_list = [f"param_{i}" for i in range(1, self.param_dims + 1)]
+            objective_list = [f"objective_{i}" for i in range(1, self.output_dims + 1)]
 
-        self.columns = ['creation_timestamp'] + param_list + objective_list
+            self.columns = ['creation_timestamp'] + param_list + objective_list
 
-        # Write Columns to Logfile
-        log_df = pd.DataFrame(columns=self.columns)
-        log_df.to_csv(self.logfile, index=False)
+            # Write Columns to Logfile
+            log_df = pd.DataFrame(columns=self.columns)
+            log_df.to_csv(self.logfile, index=False)
 
     def read_logfile(self) -> pd.DataFrame:
         
@@ -122,7 +129,7 @@ class Mission(ABC):
         log_df.to_csv(self.logfile, index=False)
 
         # TODO: Maybe take this out and change to just a number
-        print(f"Succesfully appended {data} to {self.logfile}")
+        LOG.debug(f"Succesfully appended {data} to {self.logfile}")
         
 
     
