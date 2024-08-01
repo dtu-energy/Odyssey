b83592e736d46f864bc794d6c36667625cc6e938
pwvbutler <21472918-pwvbutler@users.noreply.gitlab.com>
2024-08-01 13:58:09 +0200

    automated acquisition function setup for singleGP navigator

--- src/odyssey/navigators/singlegp_navigator.py
+++ src/odyssey/navigators/singlegp_navigator.py
@@ -7,6 +7,12 @@ torch.set_default_dtype(torch.float64)
 # Base Navigator
 from odyssey.navigators.base_navigator import Navigator
 
+# Aquisition Functions
+from botorch.acquisition import ExpectedImprovement, qExpectedImprovement, qNoisyExpectedImprovement, NoisyExpectedImprovement
+from botorch.acquisition import ProbabilityOfImprovement, qProbabilityOfImprovement, NoisyExpectedImprovement
+from botorch.acquisition import UpperConfidenceBound, qUpperConfidenceBound
+from botorch.acquisition.analytic import LogExpectedImprovement, LogProbabilityOfImprovement
+
 # Model Fitting
 from botorch.models import SingleTaskGP
 from gpytorch.mlls import ExactMarginalLogLikelihood
@@ -14,8 +20,15 @@ from botorch.fit import fit_gpytorch_mll
 from botorch.models.transforms import Standardize
 from botorch.models.transforms import Normalize
 
+# Utils
+from odyssey.utils.utils import normalize, unnormalize, standardize, unstandardize
+
 # Acquisition Function Optimization
 from botorch.optim import optimize_acqf
+
+import logging
+LOG = logging.getLogger(__name__)
+
 
 
 class SingleGP_Navigator(Navigator):
@@ -33,10 +46,22 @@ class SingleGP_Navigator(Navigator):
     """
 
     requires_init_data = True
+    
+    acq_funcs = {
+        "expected_improvement": ExpectedImprovement,
+        "upper_confidence_bound": UpperConfidenceBound,
+        "probability_of_improvement": ProbabilityOfImprovement,
+    }
+
+    acq_func_default_params = {
+        "expected_improvement": {"best_f": 0.0},
+        "upper_confidence_bound": {'beta': 0.5},
+        "probability_of_improvement": {"best_f": 0.0},
+    }
 
     def __init__(self,
-                 acq_function_type: Type,
-                 acq_function_params: dict,
+                 acq_function_type: str = "expected_improvement",
+                 acq_function_params: dict = {},
                  *args,
                  **kwargs
         ):
@@ -58,10 +83,6 @@ class SingleGP_Navigator(Navigator):
         # Acquisition Function definition
         self.acq_function_type = acq_function_type
         self.acq_function_params = acq_function_params
-
-        # Create model and acquisition function
-        self.upgrade()
-
 
     def _upgrade(self):
 
@@ -95,33 +116,19 @@ class SingleGP_Navigator(Navigator):
         
         # TODO Add Functionality for Monte Carlo Acquisition Functions
         # FIXME Change for Monte Carlo Acquisition Functions
+
+        create_function = self.acq_funcs[self.acq_function_type]
+        params = self.acq_func_default_params[self.acq_function_type]
         
         if 'best_f' in self.acq_function_params:
             self.acq_function_params['best_f'] = self.mission.train_Y.max().item()
 
         self.acq_function_params['maximize'] = True
+        params.update(self.acq_function_params)
 
-        self.acq_function = self.acq_function_type(
-                            model=self.model,
-                            **self.acq_function_params             
-                        )
-        
-    def _trajectory(self) -> torch.Tensor:
-
-        """
-        Optimizes the acquisition function to find the next best candidate.
-
-        Returns:
-            torch.Tensor: The next best candidate.
-        """
-
-        # Remove traj bounds and keep envelope (to be tested)
-        candidate, _ = optimize_acqf(
-            acq_function = self.acq_function,
-            bounds = self.traj_bounds,
-            q = 1,
-            num_restarts = 200,
-            raw_samples = 512
+        self.acq_function = create_function(
+            model=self.model,
+            **params
         )
 
         return candidate
